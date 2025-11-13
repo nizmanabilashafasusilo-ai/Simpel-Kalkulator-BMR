6dca5e36b04ff370496909bece2f61f1b680f7e3
nizmanabilashafasusilo-ai <nizmanabilashafasusilo@gmail.com>
2025-11-13 10:59:09 +0700

    Enhance IconCard with customizable text color
    Added textColor property to customize icon and text color.

--- lib/component/icon_card.dart
+++ lib/component/icon_card.dart
@@ -1,20 +1,30 @@
-import 'package:bmr/constants.dart';// import constants
-import 'package:flutter/material.dart';// import flutter material
-// icon card widget
-class IconCard extends StatelessWidget {
-  IconCard({this.cardIcon, required this.caption});
+import 'package:bmr/constants.dart'; // import constants
+import 'package:flutter/material.dart'; // import flutter material
 
+// IconCard widget
+class IconCard extends StatelessWidget {
   final IconData? cardIcon;
   final String caption;
+  final Color textColor; // warna teks
+
+  const IconCard({
+    Key? key,
+    required this.cardIcon,
+    required this.caption,
+    this.textColor = Colors.white, // warna default putih
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Icon(cardIcon, size: 70.0),
+        Icon(cardIcon, size: 70.0, color: textColor), // warna ikon ikut teks
         const SizedBox(height: 15.0),
-        Text(caption, style: labelTextStyle),
+        Text(
+          caption,
+          style: labelTextStyle.copyWith(color: textColor), // ubah warna teks
+        ),
       ],
     );
   }
