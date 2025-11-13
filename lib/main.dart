5d871c21547eb01646a279335e10271ad7082bc5
nizmanabilashafasusilo-ai <nizmanabilashafasusilo@gmail.com>
2025-11-13 10:56:51 +0700

    Update theme colors and disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-        primaryColor: const Color(0xFF0A0E21),
+        primaryColor: const Color(0xC7CEE8),
         scaffoldBackgroundColor: const Color(0xFF0A0E21),
         appBarTheme: const AppBarTheme(
           backgroundColor: Color(0xFF0A0E21),
